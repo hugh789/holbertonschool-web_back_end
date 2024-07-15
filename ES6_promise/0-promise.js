9511deafeadd245e4e81d43aa8fb5f59e69a97d0
root <hugh.fagan123@gmail.com>
2024-07-15 18:09:49 +1000

    Forgot readme.md

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,10 +1,8 @@
 export default function getResponseFromAPI() {
     const myPromise = new Promise(() => {
       setTimeout(() => {
-        // console.log('foo');
       }, 300);
     });
   
     return myPromise;
   }
-  
