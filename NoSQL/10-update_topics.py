9f503f008addc82a77f4c4af814482f9fae60c7b
root <hugh.fagan123@gmail.com>
2024-08-21 12:04:57 +1000

    change approach

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,20 +1,8 @@
 #!/usr/bin/env python3
+"""This module contains the update_topics function"""
+
+
 def update_topics(mongo_collection, name, topics):
-    """
-    Updates the topics of a school document based on its name.
-
-    Args:
-        mongo_collection: The MongoDB collection object.
-        name: The name of the school to update.
-        topics: A list of strings representing the new topics.
-    """
-
-    result = mongo_collection.update_one(
-        {"name": name},
-        {"$set": {"topics": topics}}
-    )
-
-    if result.modified_count == 0:
-        print("No document found with the name:", name)
-    else:
-        print("Updated", result.modified_count, "document(s).")
+  """Changes all topics of a school document based on the name"""
+  updated = mongo_collection.update_many({"name": name}, {"$set" :{"topics": topics}})
+  return updated
