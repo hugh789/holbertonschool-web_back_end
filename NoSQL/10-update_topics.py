d1ca90c78a6b312385289df6a5f288f3e72797e5
root <hugh.fagan123@gmail.com>
2024-08-21 11:51:56 +1000

    Adding a Python function that changes all topics of a school document based on the name

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,19 @@
+def update_topics(mongo_collection, name, topics):
+    """
+    Updates the topics of a school document based on its name.
+
+    Args:
+        mongo_collection: The MongoDB collection object.
+        name: The name of the school to update.
+        topics: A list of strings representing the new topics.
+    """
+
+    result = mongo_collection.update_one(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
+
+    if result.modified_count == 0:
+        print("No document found with the name:", name)
+    else:
+        print("Updated", result.modified_count, "document(s).")
