22037e77fb6a7b18c1e4abc78617f3e52207d75f
root <hugh.fagan123@gmail.com>
2024-09-11 10:56:28 +1000

    D

---
+++ Node_JS_basic/full_server/server.js
@@ -0,0 +1,10 @@
+// create express server (node JS & Express.js)
+import router from './routes/index';
+
+const express = require('express');
+
+const app = express();
+
+app.use('/', router).listen(1245);
+
+export default app;
